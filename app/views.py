1941f6d0c5e8f69e5a7785f5e357ec4cf668b2c8
Bahna-Darius <emanuelbahna@gmail.com>
2023-12-28 21:55:27 +0200

    URL tag Django

--- app/views.py
+++ app/views.py
@@ -1,16 +1,57 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.template import loader
 
 
-def home_page(request):
-    return HttpResponse("<h1>Home Page</h1>")
+job_title = [
+    "First Job",
+    "Second Job",
+    "Third Job"
+]
+
+job_description = [
+    "First job description",
+    "Second job description",
+    "Third job description"
+]
 
 
-def job_detail(request, id: int):
-    if id == 0:
-        return redirect("home")
+def hello(request):
+    lista = ["alpha", "beta"]
+    is_authenticated = False
+    context = {
+        "name": "Django",
+        "age": 30,
+        "first_list": lista,
+        "is_authenticated": is_authenticated
+    }
 
-    messaje = f"Job detail page {id}!"
-    return HttpResponse(messaje)
+    return render(request, "app/hello.html",context)
 
 
+def job_list(request):
+    context = {
+        "job_title_list": job_title
+    }
+
+    return render(request, "app/index.html", context)
+
+
+# Acest prim parametru din functii request el este folosit ca responsabil pentru a oferi informatia
+def job_detail(request, id):
+    print(type(id))
+
+    try:
+        if id == 0:
+            return redirect(reverse('jobs_home'))
+        # return_html = f"<h1>{job_title[id]}</h1> <h3>{job_description[id]}</h3>"
+        # return HttpResponse(return_html)
+        context = {
+            "job_title": job_title[id],
+            "job_description": job_description[id]
+        }
+        return render(request, "app/job_detail.html", context)
+    except:
+        return HttpResponseNotFound("Not Found")
